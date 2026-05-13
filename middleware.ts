df4588bce01ac2f7507491c318a4497e4a43940d
Andrian <your-github-email@example.com>
2026-05-13 20:51:39 +0700

    Fix: Add proper export to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-export const config = {
-    matcher: [
-        "/trips",
-        "/reservations",
-        "/properties",
-        "/favorites",
-    ]
-}
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const config = { 
+  matcher: [
+    "/trips",
+    "/reservations",
+    "/properties",
+    "/favorites"
+  ]
+};
